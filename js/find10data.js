17a6ddb570a3e86c85b80c75bf7225725c940f2f
enoch20050427 <enochchiu6@gmail.com>
2017-11-24 19:57:43 -0800

    Add files via upload

--- js/find10data.js
+++ js/find10data.js
@@ -6,17 +6,18 @@ function show(){
 var fdb = new ForerunnerDB();
 var myDb = fdb.db("myDb");
 var myCollection = myDb.collection("myCollection")
-myCollection.load(show);
+
 
 $("#submit").on("click",function(){
 	var newdate = $("#date").val();
 	var newkind = $("#my_select").val();
-	var newmoney= $("#Money").val();
+	var newmoney= $(".money").val();
 	var text = $("#my_select").val();
+	alert(text);
 	var newData = {
 		date : newdate,
 		kind : newkind,
-		money: newmoney
+		money: newmoney,
 	}
 	myCollection.insert(newData);
 	myCollection.save();
